87733041a87621ced9132664e95ddf2aec621373
gira patel <gira.patel94@gmail.com>
2018-11-17 15:25:28 -0500

    Add files via upload
    testing php

---
+++ footer.php
@@ -0,0 +1,8 @@
+<!--FOOTER-->  
+<footer>
+    <p class="copyright">Gira Patel © 2018</p>
+</footer>
+<!--end of content-->      
+</div>
+  </body>
+</html>
---
+++ head.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="og:description" content="Gira Patel | Visual designer with experience in graphic design, product design, and video production. Passionate about digital, branding, and interactive design. Other interests include front-end development, tech, and cinematography.">
+    <meta name="og:keywords" content="gira patel, graphic designer, digital design,  visual design, social media, video, digital designer ny, new york">
+    <title>Gira Patel</title>
+    
+    <link href="img/favicon.ico" rel="icon" type="image/bmp">
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Hind:400,500,600,700|Inconsolata:400,700">
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+	
+    <!-- Google Analytics -->
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+        ga('create', 'UA-128948376-1', 'auto');
+        ga('send', 'pageview');
+    </script>
+
+</head>
